8c767656487a49a34ab44d4ac239694ff6b73143
Yumna Asim <yumnaasim11@gmail.com>
2019-09-02 21:13:24 +0500

    program to remove junk/special characters from a string

--- src/Programs/StringManipulation.java
+++ src/Programs/StringManipulation.java
@@ -1,7 +1,30 @@
 package Programs;
 
 public class StringManipulation {
+    //this program Remove Junk/Special Chars in a String
     public static void main(String[] args) {
+        System.out.println("");
+        removeJunk();
+    }
+
+    private static void removeJunk() {
+        String sentence = "Yumna asim is $a go#od girl";
+        System.out.println(sentence);
+        System.out.println("After removing the special characters");
+        sentence =  sentence.replaceAll("\\W","");
+        System.out.println(sentence);
+    }
+
+    private static void checkChar() {
+        char ch = '$';
+        if (!(ch >= 'a' && ch <= 'z'
+                || ch >= 'A' && ch <= 'Z'
+                || ch >= 0 && ch <= 9)){
+            System.out.println(ch+" is a special character");
+        }
+    }
+
+    private static void manipulateString() {
         String name = "yumna is a girl";
         System.out.println("The result of comparing yumna with yumna is " + name.compareTo("yumna"));
         System.out.println("The result of comparing yumna with Yumna is " + name.equalsIgnoreCase("Yumna"));
