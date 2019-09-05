e30e651dc8ca417127dadc1395758476e3fd30e7
Yumna Asim <yumnaasim11@gmail.com>
2019-09-06 02:58:38 +0500

    print a number pattern

--- src/Programs/Pattern.java
+++ src/Programs/Pattern.java
@@ -2,6 +2,24 @@ package Programs;
 
 public class Pattern {
     public static void main(String[] args) {
+        printPyramid();
+        numberPattern();
+    }
+
+    private static void numberPattern() {
+        int rows = 5;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j <=i ; j++) {
+                System.out.print((j+1));
+            }
+            System.out.println(" ");
+
+        }
+    }
+
+    private static void printPyramid() {
         int rows = 5;
 
         for (int i = 0; i < rows; i++) {
